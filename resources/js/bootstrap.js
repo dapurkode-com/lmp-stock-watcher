e991b9d63d7cae611ee55b2acb7191b3ad394502
I Gusti Bagus Ngurah Satya Wibawa <i.g.b.n.satyawibawa@gmail.com>
2021-11-26 17:55:23 +0800

    tsl

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -39,7 +39,7 @@ window.Echo = new Echo({
     broadcaster: 'pusher',
     key: process.env.MIX_PUSHER_APP_KEY,
     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
-    forceTLS: false
+    forceTLS: true
 });
 
 
